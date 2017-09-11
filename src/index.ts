33e7d70d0e1511967b5596c4484d58c18163f290
Jere Aagaard <jereaa2@gmail.com>
2017-09-10 21:57:53 -0300

    Added some logs to console for some feedback on script result

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,39 @@
-import * as fse from 'fs-extra';
+import * as fse from "fs-extra";
 
-const config = JSON.parse(fse.readFileSync(__dirname + '/../config.json', 'utf8'));
-
-var filesMoved = false;
+const config = JSON.parse(fse.readFileSync(__dirname + "/../config.json", "utf8"));
 
 fse.readdir(config.defaultDir, (err, files) => {
+
+    let filesToMove = 0;
+    let fileMovesTried = 0;
+    let filesProperlyMoved = 0;
+
     files.forEach((file) => {
-        if (file.endsWith('.zip')) {
+        if (file.endsWith(".zip")) {
             config.mangaList.forEach((manga) => {
                 const regex = new RegExp(manga.regex);
                 if (regex.test(file)) {
+                    filesToMove++;
                     const srcPath = `${config.defaultDir}/${file}`;
                     const destPath = `${manga.dir}/${file}`;
-                    fse.move(srcPath, destPath, (err) => {
-                        if (err) {
-                            console.error('An error has occurred: ' + err.message);
+                    fse.move(srcPath, destPath, (error) => {
+                        fileMovesTried++;
+                        if (error) {
+                            console.error("An error has occurred: " + error.message);
                         } else {
-                            filesMoved = true;
-                            console.log(`Moved file '${file}' to dir '${manga.dir}'`);
+                            filesProperlyMoved++;
+                            console.log(`Moved file '${file}' to dir '${manga.dir}'.`);
+                        }
+
+                        if (filesToMove === fileMovesTried) {
+                            console.log(`Successfully moved ${filesProperlyMoved} out of ${fileMovesTried} files.`);
                         }
                     });
                 }
             });
         }
     });
-    if (!filesMoved) {
-        console.log("No manga files were found.");
+    if (filesToMove === 0) {
+        console.log("No files were found to move.");
     }
 });
